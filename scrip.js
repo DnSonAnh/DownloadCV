a8a115ba50563fadfa9a70f9cfbec5752ef5ac79
Brian <dnsonanh@gmail.com>
2023-04-10 13:31:31 +0700

    finanl

---
+++ scrip.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings:["Coder Bỏ nghề","............","Coder bỏ nghề"],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
